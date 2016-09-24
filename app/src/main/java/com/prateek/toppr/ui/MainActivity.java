8b9898bc6064c40fe76c6b018965770e29b5076b
Prateek Kesarwani <prateek.kesarwani@olacabs.com>
2016-09-25 00:51:53 +0530

    Iteration 1

--- app/src/main/java/com/prateek/toppr/ui/MainActivity.java
+++ app/src/main/java/com/prateek/toppr/ui/MainActivity.java
@@ -2,10 +2,12 @@ package com.prateek.toppr.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.prateek.toppr.R;
 
@@ -39,9 +41,31 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         menuItem.setChecked(true);
                         mDrawerLayout.closeDrawers();
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_home:
+                                Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_favourites:
+                                Toast.makeText(MainActivity.this, "FAV", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_statistics:
+                                Toast.makeText(MainActivity.this, "Stats", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+
                         return true;
                     }
                 });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
